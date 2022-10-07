5aba8d47d84bbc0e6f83723937415f621c477d5f
asarvazyan <areg.sarvazyan@gmail.com>
2022-10-07 19:26:08 +0200

    Refactor and add most P4 functionality

---
+++ practica4/Robot.js
@@ -0,0 +1,274 @@
+/**
+ * main.js
+ * 
+ * Robot Geometry
+ * @author <arsar1@inf.upv.es>
+ *
+ */
+
+class Robot {
+    constructor(material){
+        this.material = material;
+        this.geometry_meshes = []; // objects with meshes
+        this.geometry = this.loadRobot();
+    }
+
+    setMaterial(material) {
+        this.material = material;
+        this.updateMaterials();
+    }
+
+    updateMaterials() {
+        this.geometry_meshes.forEach(geometry => {
+            geometry.material = this.material;
+        })
+    }
+
+    loadRobot() {
+        robot = new THREE.Object3D();
+        const base = this.loadBase();
+
+        robot.add(base);
+
+        return robot;
+    }
+
+    loadBase() {
+        const base = new THREE.Mesh(new THREE.CylinderGeometry(50, 50, 15, 32, 32), this.material);
+
+        this.geometry_meshes.push(base);
+
+        base.name = "base";
+
+        base.position.set(0, 7.5, 0);
+        
+        const arm = this.loadArm();
+        base.add(arm);
+
+
+        return base;
+    }
+
+    loadArm() {
+        const arm = new THREE.Object3D();
+        const armBase = new THREE.Mesh(new THREE.CylinderGeometry(20, 20, 18, 32, 32), this.material);
+        const armTendon = new THREE.Mesh(new THREE.BoxGeometry(18, 120, 12, 32, 32), this.material);
+        const armTop = new THREE.Mesh(new THREE.SphereGeometry(20, 32, 32), this.material);
+
+        this.geometry_meshes.push(armBase);
+        this.geometry_meshes.push(armTop);
+        this.geometry_meshes.push(armTendon);
+    
+        arm.name = "arm"
+        armBase.name = "armBase";
+        armTop.name = "armTop";
+        armTendon.name = "armTendon";
+        
+        armBase.position.set(0, 0, 0);
+        armBase.rotation.z = -Math.PI / 2;
+        armTendon.position.set(0, 60, 0);
+        armTop.position.set(0, 120, 0);
+        
+        arm.add(armBase);
+        arm.add(armTendon);
+        arm.add(armTop);
+        arm.position.set(0, 0, 0);
+
+        const forearm = this.loadForearm();
+        arm.add(forearm);
+
+
+        return arm;
+    }
+
+    loadForearm() {
+        const forearm = new THREE.Object3D();
+        const forearmBase = new THREE.Mesh(new THREE.CylinderGeometry(22, 22, 6, 32, 32), this.material);
+        const forearmNerve1 = new THREE.Mesh(new THREE.BoxGeometry(4, 80, 4, 32, 32), this.material);
+        const forearmNerve2 = new THREE.Mesh(new THREE.BoxGeometry(4, 80, 4, 32, 32), this.material);
+        const forearmNerve3 = new THREE.Mesh(new THREE.BoxGeometry(4, 80, 4, 32, 32), this.material);
+        const forearmNerve4 = new THREE.Mesh(new THREE.BoxGeometry(4, 80, 4, 32, 32), this.material);
+        const forearmTop = new THREE.Mesh(new THREE.CylinderGeometry(15, 15, 40, 32, 32), this.material);
+        
+        this.geometry_meshes.push(forearmBase);
+        this.geometry_meshes.push(forearmNerve1);
+        this.geometry_meshes.push(forearmNerve2);
+        this.geometry_meshes.push(forearmNerve3);
+        this.geometry_meshes.push(forearmNerve4);
+        this.geometry_meshes.push(forearmTop);
+
+        forearmNerve1.name = "forearmNerve1";
+        forearmNerve2.name = "forearmNerve2";
+        forearmNerve3.name = "forearmNerve3";
+        forearmNerve4.name = "forearmNerve4";
+        forearmTop.name = "forearmTop";
+        forearmBase.name = "forearmBase";
+        forearm.name = "forearm";
+
+        forearmNerve1.position.set( 10, 40,  10);
+        forearmNerve2.position.set( 10, 40, -10);
+        forearmNerve3.position.set(-10, 40,  10);
+        forearmNerve4.position.set(-10, 40, -10);
+        forearmTop.position.set(0, 80, 0);
+        forearmTop.rotation.x = Math.PI / 2;
+       
+        forearm.add(forearmBase);
+        forearm.add(forearmNerve1);
+        forearm.add(forearmNerve2);
+        forearm.add(forearmNerve3);
+        forearm.add(forearmNerve4);
+        forearm.add(forearmTop);
+        forearm.position.set(0, 120, 0);
+        
+        const clamp = this.loadClamp();
+        forearm.add(clamp);
+
+        return forearm;
+    }
+
+    loadClamp() {
+        const clamp = new THREE.Object3D();
+        const clampBoxLeft  = new THREE.Mesh(new THREE.BoxGeometry(19, 20, 4, 3, 3), this.material);
+        const clampBoxRight = new THREE.Mesh(new THREE.BoxGeometry(19, 20, 4, 3, 3), this.material);
+        clampBoxLeft.position.set(  16, 0, -15);
+        clampBoxRight.position.set( 16, 0,  15);
+
+        clamp.add(clampBoxLeft);
+        clamp.add(clampBoxRight);
+        clamp.position.set(0, 80, 0);
+        
+        const clampTipGeometry = this.loadClampTipGeometry();
+
+        const clampTipLeft = new THREE.Mesh(clampTipGeometry, this.material);
+        const clampTipRight = new THREE.Mesh(clampTipGeometry, this.material);
+       
+        this.geometry_meshes.push(clampBoxLeft);
+        this.geometry_meshes.push(clampBoxRight);
+        this.geometry_meshes.push(clampTipLeft);
+        this.geometry_meshes.push(clampTipRight);
+
+        clampTipLeft.name = "clampTipLeft";
+        clampTipRight.name = "clampTipRight";
+        clampBoxLeft.name = "clampBoxLeft";
+        clampBoxRight.name = "clampBoxRight";
+        clamp.name = "clamp";
+
+        clampTipLeft.position.set( 35, 0, -15);
+        clampTipRight.position.set(35, 0,  15);
+        clampTipRight.rotation.x = Math.PI;
+
+        clamp.add(clampTipRight);
+        clamp.add(clampTipLeft);
+        
+        return clamp;
+    }
+
+    loadClampTipGeometry() {
+        const clampTipGeometry = new THREE.BufferGeometry();
+        
+        const clampTipVertices = new Float32Array([
+            // Front 
+             9.5,  -5,  0, // Bottom right small
+             9.5,   5,  0, // Top right small
+             9.5,  -5,  2, // Bottom left small
+             9.5,   5,  2, // Top left small
+            
+            // Right
+            -9.5, -10, -2, // Bottom right big
+             9.5,  -5,  0, // Bottom right small
+            -9.5,  10, -2, // Top right big
+             9.5,   5,  0, // Top right small
+            // Back
+            -9.5, -10, -2, // Bottom right big
+            -9.5,  10, -2, // Top right big
+            -9.5, -10,  2, // Bottom left big
+            -9.5,  10,  2, // Top left big
+            // Left
+            -9.5, -10,  2, // Bottom left big
+             9.5,  -5,  2, // Bottom left small
+            -9.5,  10,  2, // Top left big
+             9.5,   5,  2, // Top left small
+            // Top
+            -9.5,  10, -2, // Top right big
+             9.5,   5,  0, // Top right small
+            -9.5,  10,  2, // Top left big
+             9.5,   5,  2, // Top left small
+            // Bottom
+            -9.5, -10, -2, // Bottom right big
+             9.5,  -5,  0, // Bottom right small
+            -9.5, -10,  2, // Bottom left big
+             9.5,  -5,  2, // Bottom left small
+        ]);
+
+        const normales = new Float32Array([
+            // Front
+            1, 0, 0,
+            1, 0, 0,
+            1, 0, 0,
+            1, 0, 0,
+            
+            // Right
+            0, 0, -1,
+            0, 0, -1,
+            0, 0, -1,
+            0, 0, -1,
+
+            // Back
+            -1, 0, 0,
+            -1, 0, 0,
+            -1, 0, 0,
+            -1, 0, 0,
+
+            // Left
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+            
+            // Top
+            0, 1, 0,
+            0, 1, 0,
+            0, 1, 0,
+            0, 1, 0,
+
+            // Bottom
+            0, -1, 0,
+            0, -1, 0,
+            0, -1, 0,
+            0, -1, 0,
+        ]);
+        
+
+        const clampTipIndices = [
+            // Front
+            0, 1, 2,
+            3, 2, 1,
+            // Right
+            4, 6, 5,
+            7, 5, 6,
+            // Back
+            8, 10, 9,
+            11, 9, 10,
+            // Left
+            12, 13, 14,
+            15, 14, 13,
+            // Top
+            16, 18, 17,
+            19, 17, 18,
+            // Bottom
+            20, 21, 22, 
+            23, 22, 21,
+        ];
+
+        clampTipGeometry.setIndex(clampTipIndices);
+        clampTipGeometry.setAttribute('position', new THREE.BufferAttribute(clampTipVertices, 3));
+        clampTipGeometry.setAttribute('normal', new THREE.BufferAttribute(normales, 3));
+        
+        return clampTipGeometry;
+    }
+
+
+}
+
+
+//export {Robot};
--- practica4/main.js
+++ practica4/main.js
@@ -18,11 +18,6 @@ let robot_attrs = {
     x_speed: 5,
 }
 
-init();
-loadScene();
-setupGUI();
-render();
-
 const KEYS = {
     ARROW_LEFT: 37,
     ARROW_UP: 38,
@@ -31,247 +26,34 @@ const KEYS = {
     SPACE: 32,
 }
 
+init();
+loadScene();
+setupGUI();
+render();
+
+
 function to_rad(deg) {
     return deg / 180 * Math.PI;
 }
 
 function onDocumentKeyDown(event) {
-    console.log(event.which);
     var key_code = event.which;
     
     if (key_code == KEYS.ARROW_DOWN) {
-        robot.position.z += robot_attrs.z_speed;
+        robot.geometry.position.z += robot_attrs.z_speed;
     } else if (key_code == KEYS.ARROW_UP) {
-        robot.position.z -= robot_attrs.z_speed;
+        robot.geometry.position.z -= robot_attrs.z_speed;
     } else if (key_code == KEYS.ARROW_LEFT) {
-        robot.position.x -= robot_attrs.x_speed;
+        robot.geometry.position.x -= robot_attrs.x_speed;
     } else if (key_code == KEYS.ARROW_RIGHT) {
-        robot.position.x += robot_attrs.x_speed;
+        robot.geometry.position.x += robot_attrs.x_speed;
     } else if (key_code == KEYS.SPACE) {
-        robot.position.set(0, 0, 0);
+        robot.geometry.position.set(0, 0, 0);
     }
 }
 
-
-function loadClampTipGeometry() {
-    const clampTipGeometry = new THREE.BufferGeometry();
-    
-    const clampTipVertices = new Float32Array([
-        // Front 
-         9.5,  -5,  0, // Bottom right small
-         9.5,   5,  0, // Top right small
-         9.5,  -5,  2, // Bottom left small
-         9.5,   5,  2, // Top left small
-        
-        // Right
-        -9.5, -10, -2, // Bottom right big
-         9.5,  -5,  0, // Bottom right small
-        -9.5,  10, -2, // Top right big
-         9.5,   5,  0, // Top right small
-        // Back
-        -9.5, -10, -2, // Bottom right big
-        -9.5,  10, -2, // Top right big
-        -9.5, -10,  2, // Bottom left big
-        -9.5,  10,  2, // Top left big
-        // Left
-        -9.5, -10,  2, // Bottom left big
-         9.5,  -5,  2, // Bottom left small
-        -9.5,  10,  2, // Top left big
-         9.5,   5,  2, // Top left small
-        // Top
-        -9.5,  10, -2, // Top right big
-         9.5,   5,  0, // Top right small
-        -9.5,  10,  2, // Top left big
-         9.5,   5,  2, // Top left small
-        // Bottom
-        -9.5, -10, -2, // Bottom right big
-         9.5,  -5,  0, // Bottom right small
-        -9.5, -10,  2, // Bottom left big
-         9.5,  -5,  2, // Bottom left small
-    ]);
-
-    const normales = new Float32Array([
-        // Front
-        1, 0, 0,
-        1, 0, 0,
-        1, 0, 0,
-        1, 0, 0,
-        
-        // Right
-        0, 0, -1,
-        0, 0, -1,
-        0, 0, -1,
-        0, 0, -1,
-
-        // Back
-        -1, 0, 0,
-        -1, 0, 0,
-        -1, 0, 0,
-        -1, 0, 0,
-
-        // Left
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
-        
-        // Top
-        0, 1, 0,
-        0, 1, 0,
-        0, 1, 0,
-        0, 1, 0,
-
-        // Bottom
-        0, -1, 0,
-        0, -1, 0,
-        0, -1, 0,
-        0, -1, 0,
-    ]);
-    
-
-    const clampTipIndices = [
-        // Front
-        0, 1, 2,
-        3, 2, 1,
-        // Right
-        4, 6, 5,
-        7, 5, 6,
-        // Back
-        8, 10, 9,
-        11, 9, 10,
-        // Left
-        12, 13, 14,
-        15, 14, 13,
-        // Top
-        16, 18, 17,
-        19, 17, 18,
-        // Bottom
-        20, 21, 22, 
-        23, 22, 21,
-    ];
-
-    clampTipGeometry.setIndex(clampTipIndices);
-    clampTipGeometry.setAttribute('position', new THREE.BufferAttribute(clampTipVertices, 3));
-    clampTipGeometry.setAttribute('normal', new THREE.BufferAttribute(normales, 3));
-    
-    return clampTipGeometry;
-}
-
-function loadRobotClamp() {
-    const clamp = new THREE.Object3D();
-    const leftClampBox  = new THREE.Mesh(new THREE.BoxGeometry(19, 20, 4, 3, 3), robot_material);
-    const rightClampBox = new THREE.Mesh(new THREE.BoxGeometry(19, 20, 4, 3, 3), robot_material);
-    leftClampBox.position.set(  16, 0, -15);
-    rightClampBox.position.set( 16, 0,  15);
-
-    clamp.add(leftClampBox);
-    clamp.add(rightClampBox);
-    clamp.position.set(0, 80, 0);
-    
-    const clampTipGeometry = loadClampTipGeometry();
-
-    const clampTipLeft = new THREE.Mesh(clampTipGeometry, robot_material);
-    const clampTipRight = new THREE.Mesh(clampTipGeometry, robot_material);
-    
-    clampTipLeft.name = "clampTipLeft"
-    clampTipRight.name = "clampTipRight"
-    leftClampBox.name = "leftClampBox"
-    rightClampBox.name = "rightClampBox"
-
-    clampTipLeft.position.set( 35, 0, -15);
-    clampTipRight.position.set(35, 0,  15);
-    clampTipRight.rotation.x = Math.PI;
-
-    clamp.add(clampTipRight);
-    clamp.add(clampTipLeft);
-
-    return clamp;
-}
-
-function loadRobotForearm() {
-    const forearm = new THREE.Object3D();
-    const forearmBase = new THREE.Mesh(new THREE.CylinderGeometry(22, 22, 6, 32, 32), robot_material);
-    const forearmNerve1 = new THREE.Mesh(new THREE.BoxGeometry(4, 80, 4, 32, 32), robot_material);
-    const forearmNerve2 = new THREE.Mesh(new THREE.BoxGeometry(4, 80, 4, 32, 32), robot_material);
-    const forearmNerve3 = new THREE.Mesh(new THREE.BoxGeometry(4, 80, 4, 32, 32), robot_material);
-    const forearmNerve4 = new THREE.Mesh(new THREE.BoxGeometry(4, 80, 4, 32, 32), robot_material);
-    const forearmTop = new THREE.Mesh(new THREE.CylinderGeometry(15, 15, 40, 32, 32), robot_material);
-    
-    forearmNerve1.position.set( 10, 40,  10);
-    forearmNerve2.position.set( 10, 40, -10);
-    forearmNerve3.position.set(-10, 40,  10);
-    forearmNerve4.position.set(-10, 40, -10);
-    forearmTop.position.set(0, 80, 0);
-    forearmTop.rotation.x = Math.PI / 2;
-   
-    forearm.add(forearmBase);
-    forearm.add(forearmNerve1);
-    forearm.add(forearmNerve2);
-    forearm.add(forearmNerve3);
-    forearm.add(forearmNerve4);
-    forearm.add(forearmTop);
-    forearm.position.set(0, 120, 0);
-    
-    const clamp = loadRobotClamp();
-    forearm.add(clamp);
-
-    clamp.name = "clamp"
-
-    forearmNerve1.name = "forearmNerve1"
-    forearmNerve2.name = "forearmNerve2"
-    forearmNerve3.name = "forearmNerve3"
-    forearmNerve4.name = "forearmNerve4"
-    forearmTop.name = "forearmTop"
-    forearmBase.name = "forearmBase"
-
-    return forearm;
-}
-
-function loadRobotArm() {
-    const arm = new THREE.Object3D();
-    const armBase = new THREE.Mesh(new THREE.CylinderGeometry(20, 20, 18, 32, 32), robot_material);
-    const armTendon = new THREE.Mesh(new THREE.BoxGeometry(18, 120, 12, 32, 32), robot_material);
-    const armTop = new THREE.Mesh(new THREE.SphereGeometry(20, 32, 32), robot_material);
-    
-    armBase.position.set(0, 0, 0);
-    armBase.rotation.z = -Math.PI / 2;
-    armTendon.position.set(0, 60, 0);
-    armTop.position.set(0, 120, 0);
-    
-    armBase.name = "armBase"
-    armTop.name = "armTop"
-    armTendon.name = "armTendon"
-    arm.name = "arm"
-
-    arm.add(armBase);
-    arm.add(armTendon);
-    arm.add(armTop);
-    arm.position.set(0, 0, 0);
-
-    const forearm = loadRobotForearm();
-    arm.add(forearm);
-
-    return arm;
-}
-
-function loadRobotBase() {
-    const base = new THREE.Mesh(new THREE.CylinderGeometry(50, 50, 15, 32, 32), robot_material);
-    base.position.set(0, 7.5, 0);
-    base.name = "base";
-
-    const arm = loadRobotArm();
-    base.add(arm);
-
-    return base;
-}
-
-function loadRobot() {
-    robot = new THREE.Object3D();
-    const base = loadRobotBase();
-
-    robot.add(base);
-
-    return robot;
+function animate() {
+    console.log("Animate!");
 }
 
 function setupGUI() {
@@ -281,9 +63,9 @@ function setupGUI() {
         rotationY_forearm: 0,
         rotationZ_forearm: 0,
         rotation_clamp: 0,
-        separation_clamp: 0,
+        separation_clamp: 10,
         toggle_wire_solid: false,
-        animate: function() { console.log("Animate!"); },
+        animate: animate,
     };
 
     const gui = new lil.GUI();
@@ -334,57 +116,45 @@ function init() {
 
 function loadScene() {
     robot_material = new THREE.MeshNormalMaterial({wireframe: is_wire, flatShading: is_flatshade});
-    loadRobot();
+    robot = new Robot(robot_material);
+
     const floorMaterial = new THREE.MeshNormalMaterial({wireframe: is_wire, flatShading: is_flatshade});
-    
     const floor = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000, 100, 100), floorMaterial);
     floor.rotation.x = -Math.PI/2;
     floor.position.set(0, 0, 0);
     
-    robot.rotation.y = Math.PI / 2; // so it looks good on ortho camera
-    scene.add(robot);
+    robot.geometry.rotation.y = Math.PI / 2; // so it looks good on ortho camera
+    scene.add(robot.geometry);
     scene.add(floor);
-    console.log(robot.children);
 }
 
-function setRobotMaterial() {
-    robot_material = new THREE.MeshNormalMaterial({wireframe: is_wire, flatShading: is_flatshade});
+function updateRobot() {
+    robot.geometry.getObjectByName("base").rotation.y = to_rad(animation_controller.rotation_base);
+    robot.geometry.getObjectByName("arm").rotation.z = to_rad(animation_controller.rotation_arm);
+    robot.geometry.getObjectByName("forearm").rotation.y = to_rad(animation_controller.rotationY_forearm);
+    robot.geometry.getObjectByName("forearm").rotation.z = to_rad(animation_controller.rotationZ_forearm);
+    robot.geometry.getObjectByName("clamp").rotation.z = to_rad(animation_controller.rotation_clamp);
+    
 
-    robot.getObjectByName("base").material = robot_material;
-    robot.getObjectByName("armTop").material = robot_material;
-    robot.getObjectByName("armTendon").material = robot_material;
-    robot.getObjectByName("armBase").material = robot_material;
-    robot.getObjectByName("forearmBase").material = robot_material;
-    robot.getObjectByName("forearmNerve1").material = robot_material;
-    robot.getObjectByName("forearmNerve2").material = robot_material;
-    robot.getObjectByName("forearmNerve3").material = robot_material;
-    robot.getObjectByName("forearmNerve4").material = robot_material;
-    robot.getObjectByName("forearmTop").material = robot_material;
-    robot.getObjectByName("leftClampBox").material = robot_material;
-    robot.getObjectByName("rightClampBox").material = robot_material;
-    robot.getObjectByName("clampTipRight").material = robot_material;
-    robot.getObjectByName("clampTipLeft").material = robot_material;
+    robot.geometry.getObjectByName("clampTipLeft").position.z = -animation_controller.separation_clamp;
+    robot.geometry.getObjectByName("clampBoxLeft").position.z = -animation_controller.separation_clamp;
+    robot.geometry.getObjectByName("clampTipRight").position.z = animation_controller.separation_clamp;
+    robot.geometry.getObjectByName("clampBoxRight").position.z = animation_controller.separation_clamp;
 }
-
 
 function update() {
-    ortho_top_camera.position.set(robot.position.x, 600, robot.position.z);
-    is_wire = animation_controller.toggle_wire_solid;
-    setRobotMaterial();
-    robot.getObjectByName("base").rotation.y = to_rad(animation_controller.rotation_base);
+    ortho_top_camera.position.set(robot.geometry.position.x, 600, robot.geometry.position.z);
 
-    /*
-    animation_controller = {
-        rotation_base: 0,
-        rotation_arm: 0,
-        rotationY_forearm: 0,
-        rotationZ_forearm: 0,
-        rotation_clamp: 0,
-        separation_clamp: 0,
-        toggle_wire_solid: false,
-        animate: function() { console.log("Animate!"); },
-    };
-    */
+    if (is_wire != animation_controller.toggle_wire_solid) {
+        is_wire = animation_controller.toggle_wire_solid;
+    }
+
+    robot_material = new THREE.MeshNormalMaterial({wireframe: is_wire, flatShading: is_flatshade});
+    robot.setMaterial(robot_material);
+
+    updateRobot() 
+
+
 }
 
 function render() {
